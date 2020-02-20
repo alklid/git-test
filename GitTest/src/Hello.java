5d07af81b9c7e95a24b3896804fe843e46a17ea8
changha.choi <alklid@gmail.com>
2020-02-20 22:25:21 +0900

    col-1124 complete

--- GitTest/src/Hello.java
+++ GitTest/src/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("git-test1");
-		Syttem.out.println("git-test2");
+		System.out.println("git-test2");
+        System.out.println("col-1124 complete");
 	}
 
 }
